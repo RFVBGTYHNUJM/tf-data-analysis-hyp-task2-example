e9e414dc24cc4cf63eebcccd8ea37368a3c13a4e
RFVBGTYHNUJM <129897748+RFVBGTYHNUJM@users.noreply.github.com>
2023-04-13 15:33:41 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,5 @@ import numpy as np
 chat_id = 813595623 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    f = 0
-    if stats.ks_2samp(x, y)>0 :
-        f = 1
-    return f # Ваш ответ, True или False
+    
+    return stats.ks_2samp(x, y)>0 # Ваш ответ, True или False
